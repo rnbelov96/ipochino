2558a9dc7845c50dc7e40ba28caaaece821ee211
Belov Roman <rnbelov96@gmail.com>
2022-01-25 16:02:02 +0300

    Change bitrix send status

--- src/ts/f5leads-init.js
+++ src/ts/f5leads-init.js
@@ -1,6 +1,6 @@
 document.f5leads = {};
 document.f5leads.emails = 'kia117@yandex.ru, Yaplusya.info@yandex.ru, leads@conversionpro.agency'; // comma separated
-document.f5leads.send_to_bitrix = 'false';
+document.f5leads.send_to_bitrix = 'true';
 document.f5leads.expect_second_form = '0';
 document.f5leads.counterId = '';
 
